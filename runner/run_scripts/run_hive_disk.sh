d097de0127d2c8559e5339a4de8e5665e537477d
Patrick Wendell <pwendell@gmail.com>
2014-03-12 12:52:37 -0700

    Adding Apache license

--- runner/run_scripts/run_hive_disk.sh
+++ runner/run_scripts/run_hive_disk.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Copyright 2013 The Regents of The University California
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 IMPALA_HOSTS=ec2-72-44-48-9.compute-1.amazonaws.com,\
 ec2-184-73-2-80.compute-1.amazonaws.com,\
 ec2-54-226-72-108.compute-1.amazonaws.com,\
--- runner/run_scripts/run_impala.sh
+++ runner/run_scripts/run_impala.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Copyright 2013 The Regents of The University California
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 IMPALA_HOSTS=ec2-54-234-9-63.compute-1.amazonaws.com,\
 ec2-54-234-85-151.compute-1.amazonaws.com,\
 ec2-23-20-27-173.compute-1.amazonaws.com,\
