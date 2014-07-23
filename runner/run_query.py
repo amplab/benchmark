70d10baec9df7cd92a4f24b1a09db18200e39064
Zongheng Yang <zongheng.y@gmail.com>
2014-07-23 16:40:50 -0700

    Fix hive runner prefix: remove stinger settings, use hdfs as username.

--- runner/run_query.py
+++ runner/run_query.py
@@ -556,17 +556,21 @@ def run_hive_benchmark(opts):
   if opts.tez:
     # Page 6 of the following doc:
     # http://public-repo-1.hortonworks.com/HDP-LABS/Projects/Stinger/StingerTechnicalPreviewInstall.pdf
-    runner = """
-    HIVE_HOME=/opt/apache-hive-0.13.0.2.1.0.0-92-bin \
-    HIVE_CONF_DIR=$HIVE_HOME/conf \
-    PATH=$HIVE_HOME/bin:$PATH \
-    HADOOP_CLASSPATH=/opt/tez-0.2.0.2.1.0.0-92/*:/opt/tez-0.2.0.2.1.0.0-92/lib/* \
-    HADOOP_USER_CLASSPATH_FIRST=true \
-    HADOOP_USER_NAME=hive \
-    /opt/apache-hive-0.13.0.2.1.0.0-92-bin/bin/hive \
-      -i /root/benchmark/runner/tez/Stinger-Preview-Quickstart/configs/stinger.settings \
-      -hiveconf hive.optimize.tez=true
-    """
+
+    # runner = """
+    # HIVE_HOME=/opt/apache-hive-0.13.0.2.1.0.0-92-bin \
+    # HIVE_CONF_DIR=$HIVE_HOME/conf \
+    # PATH=$HIVE_HOME/bin:$PATH \
+    # HADOOP_CLASSPATH=/opt/tez-0.2.0.2.1.0.0-92/*:/opt/tez-0.2.0.2.1.0.0-92/lib/* \
+    # HADOOP_USER_CLASSPATH_FIRST=true \
+    # HADOOP_USER_NAME=hive \
+    # /opt/apache-hive-0.13.0.2.1.0.0-92-bin/bin/hive \
+      # -i /root/benchmark/runner/tez/Stinger-Preview-Quickstart/configs/stinger.settings \
+      # -hiveconf hive.optimize.tez=true
+    # """
+
+    # It seems like HADOOP_USER_NAME=hive (original) won't work. See prepare_hive_dataset() in prepare_benchmark.py.
+    runner = "HADOOP_USER_NAME=hdfs hive -hiveconf hive.execution.engine=tez"
     #query_map = TEZ_MAP
     query_map = QUERY_MAP
   else:
